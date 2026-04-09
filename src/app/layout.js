5ac2de299d86607ac5fcfec3b7ed04a67a8abb42
camellika <ocawulandari89@gmail.com>
2026-04-09 18:53:59 +0700

    Update chatbot

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ChatbotWidget from "@/components/ChatbotWidget";
+import ChatbotWidget from "@/components/ChatBotWidget";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
